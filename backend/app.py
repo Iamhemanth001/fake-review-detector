a02ad536b8eeb4659adae8ddd438027efe77530f
Hemanth Kumar <mr.hk339@gmail.com>
2026-05-07 21:43:57 +0530

    Use gunicorn production server

--- backend/app.py
+++ backend/app.py
@@ -84,6 +84,6 @@ def analyze_reviews():
     return jsonify(results)
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port)
